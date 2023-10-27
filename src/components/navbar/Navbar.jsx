d09e88c73b6d1fba06cade5e52bfe9a8112e84f3
Kylie Oliver <olivkylie@gmail.com>
2023-10-27 19:41:49 -0400

    sidebar complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,36 @@
-import "./navbar.scss"
-import {motion} from "framer-motion"
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        { /* Sidebar */}
-        <div className="wrapper">
-            <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Kylie Oliver</motion.span>
-            <div className="social">
-                <a href="https://github.com/Kylie240"><img src="/facebook.png" alt="" /></a>
-                <a href="https://linkedin.com/in/kylie-oliver"><img src="/instagram.png" alt="" /></a>
-            </div>
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Kylie Oliver
+        </motion.span>
+        <div className="social">
+          <a href="https://github.com/Kylie240">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="https://linkedin.com/in/kylie-oliver">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="https://github.com/Kylie240">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="https://linkedin.com/in/kylie-oliver">
+            <img src="/instagram.png" alt="" />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
